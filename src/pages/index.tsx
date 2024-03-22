c7e5869ea2663de296c7d88010bbffd2882bdac6
vexxvakan <marius@modlich.de>
2024-03-22 17:27:39 +0100

    fixed build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import clsx from "clsx"
 import Link from "@docusaurus/Link"
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
 import Layout from "@theme/Layout"
-import HomepageFeatures from "@site/src/components/HomepageFeatures"
 import Heading from "@theme/Heading"
 
 import styles from "./index.module.css"
@@ -17,7 +16,7 @@ function HomepageHeader() {
 				</Heading>
 				<p className='hero__subtitle'>{siteConfig.tagline}</p>
 				<div className={styles.buttons}>
-					<Link className='button button--secondary button--lg' to='/docs/intro'>
+					<Link className='button button--secondary button--lg' to='/docs/'>
 						Get Started
 					</Link>
 				</div>
